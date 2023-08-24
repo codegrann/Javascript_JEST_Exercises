87732d3946fcd8ad7a562bf7d1ad173ce14734ee
codegrann <omondihills02@gmail.com>
2023-08-24 16:57:07 +0300

    .

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,15 @@
-const removeFromArray = function (array, args) {
+const removeFromArray = function (array, ...args) {
+  let arrOfArgs = [...args];
   let newArray;
   let set;
   for (e of array) {
-    if (e == args) {
-      set = new Set(array);
-      set.delete(e);
-      newArray = Array.from(set);
+    for (d of arrOfArgs) {
+      if (!e == d) {
+        newArray.push(e);
+        // set = new Set(array);
+        // set.delete(e);
+        // newArray = Array.from(set);
+      }
     }
   }
   return newArray;
