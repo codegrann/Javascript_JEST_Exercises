86384c46e7f0979e1c25b296d5d428d3a6c837c3
codegrann <omondihills02@gmail.com>
2023-08-24 17:03:50 +0300

    skipped and passed all specs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,13 @@
 const removeFromArray = function (array, ...args) {
   let arrOfArgs = [...args];
-  let newArray;
+  let newArray = [];
   let set;
   for (e of array) {
-    for (d of arrOfArgs) {
-      if (!e == d) {
-        newArray.push(e);
-        // set = new Set(array);
-        // set.delete(e);
-        // newArray = Array.from(set);
-      }
+    if (!args.includes(e)) {
+      newArray.push(e);
+      // set = new Set(array);
+      // set.delete(e);
+      // newArray = Array.from(set);
     }
   }
   return newArray;
