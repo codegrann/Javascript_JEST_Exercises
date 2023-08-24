948765f7f01e510fbf64ded90553a0835223394f
codegrann <omondihills02@gmail.com>
2023-08-24 17:34:58 +0300

    .

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  } else if (a > b) {
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+  } else if (a < 0 || b < 0) {
+    sum + 1;
+  }
+  return sum;
 };
 
 // Do not edit below this line
