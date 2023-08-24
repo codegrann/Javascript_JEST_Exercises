c884e8896194b0c3d0869f4da1d6067e65f9a568
codegrann <omondihills02@gmail.com>
2023-08-24 16:38:20 +0300

    passed test to remove 1 argument from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, args) {
+  let newArray;
+  let set;
+  for (e of array) {
+    if (e == args) {
+      set = new Set(array);
+      set.delete(e);
+      newArray = Array.from(set);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
