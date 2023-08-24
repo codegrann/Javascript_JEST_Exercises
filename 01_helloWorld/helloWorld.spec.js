555ba0276106656041379b427c6ce283b943d7c3
codegrann <omondihills02@gmail.com>
2023-08-24 16:01:31 +0300

    .

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,7 +1,9 @@
-const helloWorld = require('./helloWorld');
+// const helloWorld = require('./helloWorld');
+// import helloWorld from "./helloWorld";
 
-describe('Hello World', function() {
-  test('says "Hello, World!"', function() {
-    expect(helloWorld()).toEqual('Hello, World!');
+describe("Hello World", function () {
+  import helloWorld from "./helloWorld";
+  test('says "Hello, World!"', function () {
+    expect(helloWorld()).toEqual("Hello, World!");
   });
 });
