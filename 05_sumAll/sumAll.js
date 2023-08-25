d818dd47234037f8f888ef7af30780e277bb6315
codegrann <omondihills02@gmail.com>
2023-08-25 15:38:34 +0300

    .

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,25 @@
 const sumAll = function (a, b) {
   let sum = 0;
+  let max = a < b ? b : a;
+  let min = a < b ? a : b;
+  if (!typeof a == Number && !typeof b == number) return "ERROR";
+
+  function ns(a, b) {
+    if (!typeof a == Number && !typeof b == Number) console.log("ERROR");
+  }
+  ns(2, 4);
+
   if (a > 0 && b > 0) {
-    if (a < b) {
-      for (let i = a; i <= b; i++) {
-        sum += i;
-      }
-    } else if (a > b) {
-      for (let i = b; i <= a; i++) {
-        sum += i;
-      }
+    for (let i = min; i <= max; i++) {
+      sum += i;
     }
   } else {
     return "ERROR";
   }
+
   return sum;
 };
+console.log(typeof (1 === Number) || typeof (2 == Number));
 
 // Do not edit below this line
 module.exports = sumAll;
