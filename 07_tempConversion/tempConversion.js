9d5a77ea3611bfcc6f64eaa04554ee2cdf287a90
codegrann <omondihills02@gmail.com>
2023-08-26 21:51:42 +0300

    passed converting from celsius to farenheit and vice vasa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,7 +2,9 @@ const convertToCelsius = function (fr) {
   return (fr - 32) / 1.8;
 };
 
-const convertToFahrenheit = function () {};
+const convertToFahrenheit = function (c) {
+  return c * 1.8 + 32;
+};
 
 // Do not edit below this line
 module.exports = {
