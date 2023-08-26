a43068539e2df36e458ae04b6c11278b1c7aec3b
codegrann <omondihills02@gmail.com>
2023-08-26 21:50:04 +0300

    .

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fr) {
+  return (fr - 32) / 1.8;
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function () {};
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
