b108e9aede98c3b2bc6cebf2e61fcf7314d357cd
codegrann <omondihills02@gmail.com>
2023-08-26 22:17:52 +0300

    .

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,6 +1,10 @@
-const add = function () {};
+const add = function (a, b) {
+  return a + b;
+};
 
-const subtract = function () {};
+const subtract = function (a, b) {
+  return a - b;
+};
 
 const sum = function () {};
 
