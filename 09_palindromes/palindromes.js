d3bb9dc716153db525e127de8a3b17d4f8ec793b
codegrann <omondihills02@gmail.com>
2023-08-27 21:47:43 +0300

    .

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let arrOfString = string.split(",");
+  let joinedString = arrOfString.join();
+  console.log(arrOfString);
+  console.log(joinedString);
 };
-
+palindromes("12 3");
 // Do not edit below this line
 module.exports = palindromes;
