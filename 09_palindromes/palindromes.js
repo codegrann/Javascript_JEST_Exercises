f473a568541bb1d0eba2b0d8602aead85556d3d8
codegrann <omondihills02@gmail.com>
2023-08-28 12:37:25 +0300

    .

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,8 +2,11 @@ const palindromes = function (string) {
   let arrOfString = string.split("");
   let reversedString = [...arrOfString].reverse();
   let joinedString = arrOfString.join();
+  let toMatch = /{a-zA-Z}/i;
   joineReversedString = reversedString.join();
-  // return joinedString==joineReversedString;
+
+  return joinedString == joineReversedString;
+
   console.log(arrOfString);
   console.log(joinedString);
   console.log(joinedString == joineReversedString);
