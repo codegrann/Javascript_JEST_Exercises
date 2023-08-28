6c2a20e415715e0b7b1271e394deba77dac4ee4a
codegrann <omondihills02@gmail.com>
2023-08-28 13:04:43 +0300

    worked with punctuation marks and single words palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,13 @@
 const palindromes = function (string) {
   let arrOfString = string.replace(/[^a-z0-9]/g, "").split("");
   let reversedString = [...arrOfString].reverse();
+  return arrOfString.join() == reversedString.join();
   let joinedString = arrOfString.join();
   joineReversedString = reversedString.join();
 
   console.log(arrOfString);
   console.log(joinedString);
-  console.log(joinedString == joineReversedString);
+  console.log(arrOfString.join() == reversedString.join());
 };
 palindromes("121");
 // Do not edit below this line
