949e71880a4a538ed3905d20f410684a906e1ac5
codegrann <omondihills02@gmail.com>
2023-08-28 13:15:19 +0300

    worked with uppercase letters and multiple words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,13 @@
 const palindromes = function (string) {
-  // let arrOfString = string.replace(/[^a-z0-9]/g, "").split("");
-  let arrOfString = string.replace(/\b/g, "").split("");
+  let arrOfString = string
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split("");
+  // let arrOfString = string.replace(/\b/gi, "").split("");
   let reversedString = [...arrOfString].reverse();
-  // return arrOfString.join() == reversedString.join();
+  return arrOfString.join() == reversedString.join();
 
-  console.log(arrOfString.join() == reversedString.join());
+  // console.log(arrOfString.join() == reversedString.join());
 };
 palindromes("121");
 // Do not edit below this line
