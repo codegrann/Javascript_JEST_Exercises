b3111b148fa44b8956311f3ee0c9dfd88708f118
codegrann <omondihills02@gmail.com>
2023-08-29 19:29:54 +0300

    .

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let array = [];
+  array[0] = 1;
+  array[1] = array[0];
+  console.log(array[1]);
+  if (n == 0) return 0;
+  if (n == 1) return 1;
 
+  for (i = 1; i <= array.length; i++) {
+    array[i] = array[i - 1] + array[i - 2];
+  }
 };
+fibonacci();
 
 // Do not edit below this line
 module.exports = fibonacci;
