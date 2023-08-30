907450dae85eec183df20ae6c09efc81e577f46a
codegrann <omondihills02@gmail.com>
2023-08-30 15:14:27 +0300

    .

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let currentYear = new Date().getFullYear();
+  console.log(currentYear);
 
+  let ageDiff = people.map((person) => {
+    person.yearOfDeath - person.yearOfBirth;
+  });
+  return currentYear;
 };
 
+// findTheOldest()
 // Do not edit below this line
 module.exports = findTheOldest;
